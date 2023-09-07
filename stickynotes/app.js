d2fc5d77f131b717cfd35fd9a50c8cb94cb0570b
qazaman <104552064+qazaman@users.noreply.github.com>
2023-09-07 17:00:44 +0100

    Add files via upload

--- stickynotes/app.js
+++ stickynotes/app.js
@@ -1,4 +1,8 @@
 let noteCount = 0;
+let url = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+
+
+
 class Note {
 
   constructor(string) {
@@ -27,6 +31,8 @@ function makeNote(string) {
 
 };
 
+
+
 function deleteSquare(noteId) {
   const div = document.getElementById(`note${noteId}`);
   const parentDiv = div.parentElement;
@@ -35,13 +41,13 @@ function deleteSquare(noteId) {
 
 function createSquare(size) {
 noteCount++;
-  const html = `
-  <div class = "note" id="note${noteCount}" >
-    <input class="buttonDelete" type="button" value="" onclick=" deleteSquare(${noteCount})" />
-    <p>${makeNote(prompt("enter note:"))}</p>
-  </div>
-  `;
-  document.querySelector("body").innerHTML += html;
+const div = document.getElementById("stickyNoteBox");
+div.insertAdjacentHTML("afterbegin", ` <div class = "note" id="note${noteCount}" >
+<input class="buttonDelete" type="button" value="" onclick=" deleteSquare(${noteCount})" />
+<p>${makeNote(prompt("enter note:"))}</p>
+</div>`);
+
+  
 }
 
 async function showAvatar() {
@@ -91,4 +97,80 @@ function showTime() {
   var timee = document.querySelector('#timee');
   timee.textContent = time;
 
+}
+
+function makeList() {
+  const div = document.getElementById("recieptList");
+let ul = document.createElement('ul');
+div.append(ul);
+
+while (true) {
+  let data = prompt("Enter the text for the list item", "");
+
+  if (!data) {
+    break;
+  }
+
+  let li = document.createElement('li');
+  li.textContent = data;
+  ul.append(li);
+}
+}
+
+async function fetchData(url) {
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const json_data = await response.json();
+    return json_data;
+    
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
+    return null;
+  }
+
+
+
+}
+
+fetchData(url)
+  .then(data2 => {
+    // Within this .then() block, you can define 'globalVariable'
+    data = data2;
+
+    // You can also use 'globalVariable' here
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+
+function createTreeText(data) {
+
+
+
+  let ul = document.createElement('ul');
+
+  for (let key in data) {
+    let li = document.createElement('li');
+    li.innerHTML = key;
+
+    let childrenUl = createTreeText(data[key]);
+    if (childrenUl) {
+      li.append(childrenUl);
+    }
+
+    ul.append(li);
+  }
+
+  return ul;
+}
+
+function createTree(container,obj) {
+  container.append(createTreeText(obj))
 }
